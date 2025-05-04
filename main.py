27c1d7b14a3113bda692809c5839be5cd15c93e0
souhilsh <sharafsouhil@gmail.com>
2025-05-04 22:18:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Willkommen beim L-Bank Förderrechner!")
+print("Willkommen bei dem Förderrechner!")
 
 # Benutzereingaben
 einkommen = float(input("Geben Sie Ihr Jahreseinkommen ein (in €): "))
